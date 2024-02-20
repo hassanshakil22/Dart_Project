bea8cbdc453bc9704914bf9d6bda1bea64e48304
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-20 15:33:15 +0500

    handling the main if user in inputs soemthing other than 1/2 and removing my dart game as 2 apps are enough

--- main.dart
+++ main.dart
@@ -5,21 +5,24 @@ import 'passsword App/password.dart';
 void main() {
   print("----------------Welcome To My dart Project----------------");
 
-  print("here is the list of things you can do on the app : \n ");
+  print("here are the  things you can do on the app : \n ");
   print("Enter '1' --> Recipe Organizer ");
   print(
       "DESCRIPTION : Add , Edit and Delete Recipes for delicious cusines \n ");
   print("Enter '2' --> Password Generator");
   print("DESCRIPTION : Generate or Save a random password : \n ");
-  print("Enter '3' --> Play a Game");
-
-  stdout.write("Enter Your Choice :");
-  String? userInput = stdin.readLineSync();
-
-  if (userInput == "1") {
-    recipeOrganizer();
-  } else if (userInput == "2") {
-    passwordApp();
-  } else if (userInput == "3") {
-  } else {}
+  // bool continuation = true;
+  while (true) {
+    stdout.write("Enter Your Choice :");
+    String? userInput = stdin.readLineSync();
+    if (userInput == "1") {
+      recipeOrganizer();
+      break;
+    } else if (userInput == "2") {
+      passwordApp();
+      break;
+    } else {
+      print("\n\nInvalid Input! Please Enter Again.\n");
+    }
+  }
 }
